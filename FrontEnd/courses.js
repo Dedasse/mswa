f55928b363a81a472d85bb8e65854e0b8f1dd134
ronihanninen <57250711+ronihanninen@users.noreply.github.com>
2020-05-03 16:10:42 +0300

    updated again

--- FrontEnd/courses.js
+++ FrontEnd/courses.js
@@ -2,18 +2,31 @@ var children =
     [
         {
             "opinto_id": 1,
-            "Period_name": "Älykkäät IoT-laitteet",
-            "course_name": "Smart"
+            "periods.name": "Älykkäät IoT-laitteet",
+            "course_name": "Smart",
+            "teacher_name": "Joseph Hotchkiss",
+            "course_info": "Kurssin aikana opiskellaan C-ohjelmointia aloittelijatasolla. Kurssin lopputyönä ohjelmoidaan monenlaisia\n" +
+                "sensoreita hyödyntävä robotti suorittamaan useita eri tehtäviä. Lopputyössä yhdistyvät muiden kurssien aikana opitut aiheet \n" +
+                "niin, että C-ohjelmointikielen lisäksi opiskelijalla tulee olla jokseenkin hallussa myös Linux, verkkotyöskentely ja \n" +
+                "digitaalitekniikan erilaiset menetelmät."
         },
         {
             "opinto_id": 1,
             "Period_name": "Älykkäät IoT-laitteet",
-            "course_name": "Verkot"
+            "course_name": "Verkot",
+            "teacher_name": "Marko Uusitalo",
+            "course_info": "Kurssin tavoitteena on tutustuttaa opiskelija verkkojen maailmaan. Aiheina kurssilla ovat niin langalliset \n" +
+                "kuin langattomatkin verkot, ja läpi käydään kaikki 4G- ja Bluetooth-yhteyksistä erilaisiin kaapelityyppeihin ja \n" +
+                "verkkojen tietoturvaan unohtamatta kuitenkaan verkkoprotokollia tai tiedonsiirron eri menetelmiä."
         },
         {
             "opinto_id": 1,
             "Period_name": "Älykkäät IoT-laitteet",
-            "course_name": "Linux"
+            "course_name": "Linux",
+            "teacher_name": "Saana Vallius",
+            "course_info": "Kurssin aikana käydään läpi Linux-perusteita, kuten käyttöjärjestelmän itsensä asennus, peruskomentoja \n" +
+                "sekä tiedosto-oikeuksien ja käyttäjien hallintaa. Kurssin päätteeksi tavoitteena on luoda yhteys Raspberry Pi -tietokoneen \n" +
+                "(tai vastaavan) ja robotin välille käyttäen MQTT-protokollaa ja WLAN-reititintä."
         },
         {
             "opinto_id": 1,
