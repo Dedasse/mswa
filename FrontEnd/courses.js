5ba29e69945a2a81f8eed4ce9d45a37a69f6a079
ronihanninen <57250711+ronihanninen@users.noreply.github.com>
2020-05-03 15:59:49 +0300

    courses.js updated

--- FrontEnd/courses.js
+++ FrontEnd/courses.js
@@ -2,33 +2,68 @@ var children =
     [
         {
             "opinto_id": 1,
-            "Period_name": "Älykkäät IoT laitteet",
+            "Period_name": "Älykkäät IoT-laitteet",
             "course_name": "Smart"
         },
         {
             "opinto_id": 1,
-            "Period_name": "Älykkäät IoT laitteet",
+            "Period_name": "Älykkäät IoT-laitteet",
             "course_name": "Verkot"
         },
         {
             "opinto_id": 1,
-            "Period_name": "Älykkäät IoT laitteet",
+            "Period_name": "Älykkäät IoT-laitteet",
             "course_name": "Linux"
         },
         {
             "opinto_id": 1,
-            "Period_name": "Älykkäät IoT laitteet",
+            "Period_name": "Älykkäät IoT-laitteet",
             "course_name": "Digitaalitekniikka"
         },
         {
+            "opinto_id": 1,
+            "Period_name": "Älykkäät IoT-laitteet",
+            "course_name": "Matematiikka"
+        },
+        {
+            "opinto_id": 1,
+            "Period_name": "Älykkäät IoT-laitteet",
+            "course_name": "Fysiikka"
+        },
+        {
+            "opinto_id": 1,
+            "Period_name": "Älykkäät IoT-laitteet",
+            "course_name": "Englanti"
+        },
+        {
             "opinto_id": 2,
-            "Period_name": "Mobiilit terveyssovellutukset",
+            "Period_name": "Mobiilit terveyssovellukset",
             "course_name": "Android"
         },
         {
             "opinto_id": 2,
-            "Period_name": "Mobiilit terveyssovellutukset",
-            "course_name": "Hyte"
+            "Period_name": "Mobiilit terveyssovellukset",
+            "course_name": "Hyvinvointiteknologia"
+        },
+        {
+            "opinto_id": 2,
+            "Period_name": "Mobiilit terveyssovellukset",
+            "course_name": "Viestintä"
+        },
+        {
+            "opinto_id": 2,
+            "Period_name": "Mobiilit terveyssovellukset",
+            "course_name": "Matematiikka"
+        },
+        {
+            "opinto_id": 2,
+            "Period_name": "Mobiilit terveyssovellukset",
+            "course_name": "Fysiikka"
+        },
+        {
+            "opinto_id": 2,
+            "Period_name": "Mobiilit terveyssovellukset",
+            "course_name": "Java"
         },
         {
             "opinto_id": 3,
@@ -43,16 +78,46 @@ var children =
         {
             "opinto_id": 3,
             "Period_name": "Pelikehitys",
-            "course_name": "Databases"
+            "course_name": "Tietokannat"
+        },
+        {
+            "opinto_id": 3,
+            "Period_name": "Pelikehitys",
+            "course_name": "Matematiikka"
+        },
+        {
+            "opinto_id": 3,
+            "Period_name": "Pelikehitys",
+            "course_name": "Fysiikka"
+        },
+        {
+            "opinto_id": 3,
+            "Period_name": "Pelikehitys",
+            "course_name": "Englanti"
         },
         {
             "opinto_id": 4,
             "Period_name": "Web-tekniikat ja digitaalinen media",
-            "course_name": "Media"
+            "course_name": "Digitaalinen media"
         },
         {
             "opinto_id": 4,
             "Period_name": "Web-tekniikat ja digitaalinen media",
-            "course_name": "Web-tekniikat"
-        }
+            "course_name": "HTML, CSS ja JavaScript"
+        },
+        {
+            "opinto_id": 4,
+            "Period_name": "Web-tekniikat ja digitaalinen media",
+            "course_name": "Matematiikka"
+        },
+        {
+            "opinto_id": 4,
+            "Period_name": "Web-tekniikat ja digitaalinen media",
+            "course_name": "Fysiikka"
+        },
+        {
+            "opinto_id": 4,
+            "Period_name": "Web-tekniikat ja digitaalinen media",
+            "course_name": "Viestintä"
+        },
     ]
